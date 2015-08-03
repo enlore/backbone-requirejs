09302b90287be3bd36773613ce2cbad95f7864b6
enlore <n.e.lorenson@gmail.com>
2015-08-03 11:50:30 -0500

    Reorder run list, set watchers

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,26 @@ var commonGulp      = require("./common-gulp")
 
 // Default run list
 var taskList = [
-    "api",
     "vendorJS",
     "vendorCSS",
     "componentStylus",
     "appTemplatesJade",
-    "appJS"
+    "appJS",
+    "apiJS",
+    "api"
 ]
 
 
-gulp.task("default", taskList, function () {})
+gulp.task("default", taskList, function () {
+  gulp.watch(["vendor/js/**/*.js", "bower_components/**/*.js"], ["vendorJS"]);
+  gulp.watch(["vendor/css/**/*.css", "bower_components/**/*.css"], ["vendorCSS"]);
+  //gulp.watch("src/client/layouts/**/*.jade", ["layouts"]);
+  gulp.watch(["src/client/**/*.js"], ["appJS"]);
+  gulp.watch(["src/client/**/*.styl"], ["componentStylus"]);
+  //gulp.watch("assets/**", ["assets"]);
+  gulp.watch("src/client/**/*.jade", ["appTemplatesJade"]);
+  gulp.watch("src/client/index.js", ["appJS"])
+})
 
 gulp.task("api", commonGulp.nodemonAPI)
 gulp.task("vendorJS", commonGulp.vendorJS)
@@ -22,6 +32,7 @@ gulp.task("vendorCSS", commonGulp.vendorCSS)
 gulp.task("componentStylus", commonGulp.componentStylus(stylusConfig))
 gulp.task("appTemplatesJade", commonGulp.appTemplatesJade)
 gulp.task("appJS", commonGulp.appJS)
+gulp.task("apiJS", commonGulp.apiJS)
 
 var stylusConfig = {}
 
